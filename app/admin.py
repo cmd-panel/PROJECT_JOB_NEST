c243e2432a81d170c827b54491873b7d6b9f38e1
wasibahmedchayan <israbonty@gmail.com>
2025-05-01 23:08:30 +0600

    error fixed and the code is working

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import JobPosting, CV, Profile, Application, Portfolio, Certificate, UserDetails, CourseVideo, Course
+from .models import JobPosting, CV, Profile, Application, Portfolio, Certificate, UserDetails, CourseVideo, Course, Message
 
 admin.site.register(JobPosting)
 
@@ -22,3 +22,5 @@ admin.site.register(UserDetails)
 admin.site.register(Course)
 
 admin.site.register(CourseVideo)
+
+admin.site.register(Message)
